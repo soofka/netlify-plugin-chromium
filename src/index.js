4f6729a86195aeb5063e29b9ec12a0d50d933dc6
ehmicky <ehmicky@gmail.com>
2020-12-15 19:35:07 +0100

    Simplify npm install chromium

--- src/index.js
+++ src/index.js
@@ -1,38 +1,13 @@
-import {
-  log,
-  getChromiumPath,
-  installChromium,
-} from './utils'
+const chromium = require("chromium");
 
-const NetlifyChromiumPlugin = {
-  onPreBuild: async ({ inputs, utils }) => {
-    log(`Installing Chromium with settings: ${JSON.stringify(inputs)}`);
-
-    try {
-      let chromiumPath;
-
-      try {
-        chromiumPath = getChromiumPath();
-      } catch(requireError) {
-        log('Chromium is not available, attempting to download');
-
-        await installChromium(utils.run.command, inputs.packageManager);
-        chromiumPath = getChromiumPath();
-      }
-
-      if (inputs.setChromePathInEnv) {
-        log(`Setting environmental variable CHROME_PATH to ${chromiumPath}`);
-        process.env.CHROME_PATH = chromiumPath;
-      }
-
-      log(`Chromium installation finished with SUCCESS (path: ${chromiumPath})`);
-    } catch(error) {
-      log('Chromium installation finished with FAILURE', error);
-
-      const failFunc = inputs.failBuildOnError ? utils.build.failBuild : utils.build.failPlugin;
-      failFunc('Error during Chromium installation', { error });
+export default {
+  async onPreBuild({ inputs }) {
+    if (inputs.setChromePathInEnv) {
+      const chromiumPath = chromium.path;
+      console.log(
+        `[NetlifyChromiumPlugin]: Setting environmental variable CHROME_PATH to ${chromiumPath}`
+      );
+      process.env.CHROME_PATH = chromiumPath;
     }
   },
 };
-
-export default NetlifyChromiumPlugin;
